c922189d60b9ae780a3d6b2d6e1758bb8fac2e67
Ankitakamthe153 <88307915+Ankitakamthe153@users.noreply.github.com>
2024-05-19 20:53:18 +0530

    created a submission

--- script.js
+++ script.js
@@ -10,11 +10,32 @@ function convertToRoman(num) {
     };
 
   //your code here
+	let roman = '';
+    let i = 0;
+
+    while (num > 0) {
+        const [symbol, value] = obj[i];
+        if (num >= value) {
+            roman += symbol;
+            num -= value;
+        } else {
+            // Check for subtractive notation (e.g., IV, IX, XL, XC, etc.)
+            const nextValue = obj[i + 1][1];
+            const subtractiveSymbol = obj[i + 1][0];
+            if (num >= value - nextValue) {
+                roman += subtractiveSymbol + symbol;
+                num -= (value - nextValue);
+            }
+        }
+        i++;
+    }
+
+    return roman;
 
 }
 // You can test your code by running the above function and printing it to console by pressing the run button at the top. To run it with input 36, uncomment the following line
 
-// console.log(convertToRoman(36));
+ //console.log(convertToRoman(36));
 
 
 
